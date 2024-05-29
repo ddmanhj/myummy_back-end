393699f1b49de5087b9c74baeb334aaa135a025c
Manhj <ddmanhj@gmail.com>
2024-05-29 23:59:43 +0700

    remove redis

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const redis = require("redis");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3000;
